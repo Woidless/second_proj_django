e96fa3843409f7ce943921f88e41aa8be11b53b7
Woidless <bmoblp@mail.ru>
2022-10-05 13:08:10 +0600

    CRUD

--- blogApi/urls.py
+++ blogApi/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('account.urls'))
-    
+    path('accounts/', include('account.urls')),
+    path('', include('main.urls')),    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # path('categories/', views.category_list), # 8000/categories/
+    # path('categories1/', views.CategoryListView.as_view()), # 8000/categories1/
+    path('categories/', views.CategoryListView.as_view()), # 8000/categories/
+    path('posts/', views.PostListCreateView.as_view()), # 8000/posts/
+    path('posts/<int:pk>/', views.PostDetailView.as_view()), # 8000/posts/<id>
+]
