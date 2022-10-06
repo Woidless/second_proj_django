9d79b1a201dc7b9a015ce457ef0293e0e30cb869
Woidless <bmoblp@mail.ru>
2022-10-06 13:01:14 +0600

    CRUD

--- main/serializers.py
+++ main/serializers.py
@@ -1,6 +1,7 @@
 from unicodedata import category
+from urllib import request
 from rest_framework import serializers
-from .models import Category, Post, Comment
+from .models import Category, Post, Comment, PostImage
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -17,25 +18,46 @@ class PostListSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'owner', 'preview')
 
 
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImage
+        exclude = ('id', 'title')
+
+
 class PostCreateSerializer(serializers.ModelSerializer):
+    images = PostImageSerializer(many=True, read_only=False, required=False)
     class Meta:
         model = Post
-        fields = ('title', 'body', 'category', 'preview',)
+        fields = ('title', 'body', 'category', 'preview', 'images')
     
+    def create(self, validated_data):
+        request = self.context.get('request')
+        post = Post.objects.create(**validated_data)
+        images_data = request.FILES.getlist('images')
+        print(images_data,'\n'+'='*100)
+        images_objects = [PostImage(post=post, image=image) for image in images_data]
+        print(images_objects, '\n'+'='*100)
+        PostImage.objects.bulk_create(images_objects)
+        return post
+
+
+class CommentSerializers(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    post = serializers.ReadOnlyField(source='post.title')
+    
+    class Meta:
+        model = Comment
+        fields = ('id', 'body', 'post', 'owner')
+
 
 class PostSerializer(serializers.ModelSerializer):
     category = serializers.ReadOnlyField(source='category.name')
     owner = serializers.ReadOnlyField(source='owner.username')
+    images = PostImageSerializer(many=True)
+    comments = CommentSerializers(many=True)
 
     class Meta:
         model = Post
         fields = '__all__'
 
 
-class CommentSerializers(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-
-    class Meta:
-        model = Comment
-        fields = ('id', 'body', 'post', 'owner')
-        
